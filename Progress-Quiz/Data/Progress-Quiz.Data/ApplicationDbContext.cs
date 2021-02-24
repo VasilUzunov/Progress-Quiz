137c9d57e93465216da606341992e56ace66a38d
Vasil Uzunov <vasil6062@gmail.com>
2021-02-24 17:05:26 +0200

    Structuring the project

---
+++ Progress-Quiz/Data/Progress-Quiz.Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Progress_Quiz.Models;
+
+namespace Progress_Quiz.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Question> Questions { get; set; }
+
+        public DbSet<Answer> Answers { get; set; }
+    }
+}
---
+++ Progress-Quiz/Data/Progress-Quiz.Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Progress_Quiz.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Category { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string QuestionText { get; set; }
+
+        public virtual Answer Answer { get; set; }
+    }
+}
