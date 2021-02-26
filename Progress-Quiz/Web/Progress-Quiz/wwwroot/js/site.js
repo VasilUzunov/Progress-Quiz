ac1b99d30c0bc6445132b834b621973970b4c112
Vasil Uzunov <vasil6062@gmail.com>
2021-02-26 15:31:26 +0200

    Creating front-end

--- Progress-Quiz/Web/Progress-Quiz/wwwroot/js/site.js
+++ Progress-Quiz/Web/Progress-Quiz/wwwroot/js/site.js
@@ -1,34 +1,164 @@
 ﻿var questions = window.questions;
 console.log(questions);
 
-let history = questions.filter(x => x.category === 'History');
-var item = history[Math.floor(Math.random() * history.length)];
+var count = 1;
+
+window.onload = function () {
+    let history = questions.filter(x => x.category === 'History');
+    var item = history[Math.floor(Math.random() * history.length)];
+
+    var h1 = document.getElementById('category');
+    var p = document.getElementById('qtext');
+    var labels = document.querySelectorAll('label');
+
+    h1.textContent = item.category;
+    p.textContent = item.questionText;
+
+    var p1 = document.createElement('p');
+    p1.textContent = item.wrongAnswers[0].answer;
+    var p2 = document.createElement('p');
+    p2.textContent = item.correctAnswer;
+    var p3 = document.createElement('p');
+    p3.textContent = item.wrongAnswers[1].answer;
+    var p4 = document.createElement('p');
+    p4.textContent = item.wrongAnswers[2].answer;
+
+    labels[0].appendChild(p1);
+    labels[1].appendChild(p2);
+    labels[2].appendChild(p3);
+    labels[3].appendChild(p4);
+
+    var oneMinute = 60,
+        display = document.querySelector('#countdown');
+    startTimer(oneMinute, display);
+};
 
 var correctAnswers = 0;
 var WrongAnswers = 0;
+
+
 document.getElementsByClassName('answerbutton')[0].addEventListener('click', (e) => { submitQuestion(e) });
 document.getElementsByClassName('answerbutton')[1].addEventListener('click', (e) => { nextQuestion(e) });
 
+var chekedLabels = document.querySelectorAll('label input');
+var label = chekedLabels[1].parentElement;
+
+var clickedLabel; 
 
 function submitQuestion(e) {
     e.preventDefault();
     var btnNext = document.getElementsByClassName('answerbutton')[1];
     btnNext.style.display = "inline";
 
-    var labels = document.querySelectorAll('label input');
+    clickedLabel = document.querySelector('input[name="quiz"]:checked').parentNode;
 
-    var clickedLabel = document.querySelector('input[name="quiz"]:checked').parentNode;
-
-    if (clickedLabel.textContent === item.correctAnswers) {
+    if (clickedLabel.id === 'label2') {
         clickedLabel.style.background = 'green';
     } else {
         clickedLabel.style.background = 'red';
-        labels.filter(x=>x.textContent===item.correctAnswer).style.background ='green'
+        label.style.background = 'green';
     }
 }
 
 function nextQuestion(e) {
     e.preventDefault();
+
+    var span = clickedLabel.children[1];
+    //span.style.backgroundColor = 'white';
+    clickedLabel.style.backgroundColor = '#ddd';
+    label.style.backgroundColor = '#ddd';
+    span.className = 'checkmark';
+
+
+    let sports = questions.filter(x => x.category === 'Sport');
+    let maths = questions.filter(x => x.category === 'Math');
+    let animals = questions.filter(x => x.category === 'Animals');
+    let technologies = questions.filter(x => x.category === 'Technology');
+
+    labels.forEach(x => x.removeChild(x.lastChild));
+
+    if (count === 1) {
+        var sport = sports[Math.floor(Math.random() * sports.length)];
+
+        h1.textContent = sport.category;
+        p.textContent = sport.questionText;
+
+        var p1 = document.createElement('p');
+        p1.textContent = sport.wrongAnswers[0].answer;
+        var p2 = document.createElement('p');
+        p2.textContent = sport.correctAnswer;
+        var p3 = document.createElement('p');
+        p3.textContent = sport.wrongAnswers[1].answer;
+        var p4 = document.createElement('p');
+        p4.textContent = sport.wrongAnswers[2].answer;
+
+        labels[0].appendChild(p1);
+        labels[1].appendChild(p2);
+        labels[2].appendChild(p3);
+        labels[3].appendChild(p4);
+    } else if (count === 2) {
+        var math = maths[Math.floor(Math.random() * maths.length)];
+
+        h1.textContent = math.category;
+        p.textContent = math.questionText;
+
+        var p1 = document.createElement('p');
+        p1.textContent = math.wrongAnswers[0].answer;
+        var p2 = document.createElement('p');
+        p2.textContent = math.correctAnswer;
+        var p3 = document.createElement('p');
+        p3.textContent = math.wrongAnswers[1].answer;
+        var p4 = document.createElement('p');
+        p4.textContent = math.wrongAnswers[2].answer;
+
+        labels[0].appendChild(p1);
+        labels[1].appendChild(p2);
+        labels[2].appendChild(p3);
+        labels[3].appendChild(p4);
+        count++;
+    } else if (count === 3) {
+        var animal = animals[Math.floor(Math.random() * animals.length)];
+
+        h1.textContent = animal.category;
+        p.textContent = animal.questionText;
+
+        var p1 = document.createElement('p');
+        p1.textContent = animal.wrongAnswers[0].answer;
+        var p2 = document.createElement('p');
+        p2.textContent = animal.correctAnswer;
+        var p3 = document.createElement('p');
+        p3.textContent = animal.wrongAnswers[1].answer;
+        var p4 = document.createElement('p');
+        p4.textContent = animal.wrongAnswers[2].answer;
+
+        labels[0].appendChild(p1);
+        labels[1].appendChild(p2);
+        labels[2].appendChild(p3);
+        labels[3].appendChild(p4);
+    } else if (count === 4) {
+        var technology = technologies[Math.floor(Math.random() * technologies.length)];
+
+        h1.textContent = technology.category;
+        p.textContent = technology.questionText;
+
+        var p1 = document.createElement('p');
+        p1.textContent = technology.wrongAnswers[0].answer;
+        var p2 = document.createElement('p');
+        p2.textContent = technology.correctAnswer;
+        var p3 = document.createElement('p');
+        p3.textContent = technology.wrongAnswers[1].answer;
+        var p4 = document.createElement('p');
+        p4.textContent = technology.wrongAnswers[2].answer;
+
+        labels[0].appendChild(p1);
+        labels[1].appendChild(p2);
+        labels[2].appendChild(p3);
+        labels[3].appendChild(p4);
+    }
+    count++;
+    var oneMinute = 60,
+        display = document.querySelector('#countdown');
+    startTimer(oneMinute, display);
 }
 
 function startTimer(duration, display) {
@@ -51,35 +181,12 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
-window.onload = function () {
-    var h1 = document.getElementById('category');
-    var p = document.getElementById('qtext');
-    var labels = document.querySelectorAll('label');
-
-    var test = document.createElement('input');
-    test.type = 'radio';
-    var span = document.createElement('span');
-    span.className = 'chekmark';
-
-    labels[0].textContent += item.wrongAnswers[0].answer;
-    labels[0].appendChild(test);
-    labels[0].appendChild(span);
-    labels[1].textContent += item.correctAnswer;
-    labels[1].appendChild(test);
-    labels[2].textContent += item.wrongAnswers[1].answer;
-    labels[2].appendChild(test);
-    labels[3].textContent += item.wrongAnswers[2].answer;
-    labels[3].appendChild(test);
-
-    h1.textContent += item.category;
-    p.textContent += item.questionText;
-
-    var oneMinute = 60,
-        display = document.querySelector('#countdown');
-    startTimer(oneMinute, display);
-};
-
 function afterTheTimerBecomeZero(parameters) {
+    var btnSubmit = document.getElementsByClassName('answerbutton')[0];
+    btnSubmit.style.display = "none";
+    var btnNext = document.getElementsByClassName('answerbutton')[1];
+    btnNext.style.display = "inline";
+
     var labels = document.querySelectorAll('label input');
     console.log(labels);
 }
